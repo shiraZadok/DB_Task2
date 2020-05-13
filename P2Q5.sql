56515d8504d1e273c645100c42efe7795edb149e
shiraZadok <58064644+shiraZadok@users.noreply.github.com>
2020-05-13 14:38:05 +0300

    fix answer

--- P2Q5.sql
+++ P2Q5.sql
@@ -1,3 +1,3 @@
 select doctor_name, salary
-from doctors inner join queue_summary
-where (cast(date_ as date)='2020-04-20' and num_of_Patient>4);
+from doctors as `do` inner join queue_summary as `qu`
+where `do`.doctor_id = `qu`.doctor_id and (cast(date_ as date)='2020-04-20' and num_of_Patient>4);
